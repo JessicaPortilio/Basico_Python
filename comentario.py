f35a604b80540b3fb58269feeec46d8b6c6a3827
JessicaPortilio <jessicaket91@gmail.com>
2022-10-20 18:39:59 -0300

    Comentário em python.

---
+++ comentario.py
@@ -0,0 +1,15 @@
+# Comentários em Python:
+# Comentários em Python começam com o caractere cerquinha '#' e estende até o final da linha.
+nome = 'Jessica' # Aqui temos uma variável chamada nome que recebe uma String com o valor Jessica
+
+print(nome) # print é uma função ao qual ultilizamos para imprimir as coisas na tela.
+
+# Um comentário pode aparecer no inicio da linha ou após espaço em branco ou código
+
+ # Antes de alguma coisa 
+ # Não podemos ter comentário dentro de uma String
+texto = "#Isso não é comentário porque está entre aspas" # depois de alguma coisa
+
+print(texto)
+
+
