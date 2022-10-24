8e4d218df37b17464165963edaf6795ea33c3a02
JessicaPortilio <jessicaket91@gmail.com>
2022-10-24 14:41:49 -0300

    Operações matemáticas

---
+++ operacoesMatematicas.py
@@ -0,0 +1,30 @@
+# Vamos usar python como uma calculadora
+
+# VocÇe pode digitar uma expressão e o resultado será apresentado
+# A sintaxe de expressões é usal:
+# operadores +, -, * e /
+# parêntes (()) podem ser usados para agrupar expressões.
+
+print(2 + 2)
+
+print(5 - 2)
+
+print(5 * 2)
+
+print(10 / 2)
+
+print((50 - 5 * 6) / 4)
+
+# Para fazer a divisão obter um resultado inteiro, podemos usar o operador //
+
+print(17 / 3) # divisão clássica retornar um float
+print(17 // 3) # divisão descartar a parte francionária
+
+# para calcular o restante você pode usar %
+
+# 17 / 3
+#      5
+# 15
+#  2
+
+print(17 % 3)
