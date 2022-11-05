3d9a8709fea7e18d13ccb7978e5263a81d078ea1
JessicaPortilio <jessicaket91@gmail.com>
2022-11-05 19:17:26 -0300

    Operações Matemáticas

--- operacoesMatematicas.py
+++ operacoesMatematicas.py
@@ -1,9 +1,10 @@
 # Vamos usar python como uma calculadora
 
-# VocÇe pode digitar uma expressão e o resultado será apresentado
+# Você pode digitar uma expressão e o resultado será apresentado
 # A sintaxe de expressões é usal:
 # operadores +, -, * e /
 # parêntes (()) podem ser usados para agrupar expressões.
+
 
 print(2 + 2)
 
@@ -28,3 +29,38 @@ print(17 // 3) # divisão descartar a parte francionária
 #  2
 
 print(17 % 3)
+
+# Com python é possível usar o operador ** para calcular potências
+
+print(5 ** 3) #5 * 5 = 25 * 5 = 125
+
+# O sinal de igual ('=') é usado para atribuir um valor a uma variavel.
+
+numero1 = 10
+numero2 = 5
+
+soma = numero1 + numero2
+
+print(soma)
+
+valor = 0
+
+print(valor)
+
+nome = ''
+
+print(nome)
+
+
+
+
+nome = 'Jessica'
+
+numero1 = 15
+print(numero1)
+
+
+# True = Verdadeiro 
+# False = Falso
+
+print(10 == 15)
